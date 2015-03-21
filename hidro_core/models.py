102d54191438853a9b0f1363d0915b1eafae5571
Gabriel Romero <galexanderomero24@gmail.com>
2015-03-21 09:46:04 -0500

    Creación Equipo

--- hidro_core/models.py
+++ hidro_core/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Equipo(models.Model):
-	usuario = models.ForeignKey(User)
-	nombre = models.CharField(max_length = 100)
-	estado = models.CharField(max_length = 1)
-	fecha_creacion = models.DateTimeField(auto_now_add = True)
-	fecha_modificacion = models.DateTimeField(auto_now = True)
+    ACTIVO = 'A'
+    INACTIVO = 'I'
+    ESTADO = (
+                (ACTIVO, "ACTIVO"),
+                (INACTIVO,"INACTIVO"),
+            )
+    usuario = models.ForeignKey(User)
+    nombre = models.CharField(max_length = 100)
+    estado = models.CharField(max_length = 1, default = ACTIVO)
+    fecha_creacion = models.DateTimeField(auto_now_add = True)
+    fecha_modificacion = models.DateTimeField(auto_now = True)
 
 class EquipoMedicion(models.Model):
     equipo = models.ForeignKey(Equipo)
--- hidro_core/views.py
+++ hidro_core/views.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
+
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
@@ -13,6 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.conf import settings
 from hidro_core.models import Equipo, EquipoMedicion
+from hidro_core.forms import EquipoForm
 import json
 from django.core import serializers
 
@@ -37,12 +39,15 @@ def login_usuario(request):
 		
 	return render_to_response('login.html', context_instance=RequestContext(request))
 
+def logout(request):
+    logout(request)
+
 @login_required
 def dashboard(request):
     titulo_modulo = "Equipos "
+    usuario = None
     equipos_list = Equipo.objects.all()
     paginator = Paginator(equipos_list, 25) # Show 25 contacts per page
-
     page = request.GET.get('page')
     try:
         equipos = paginator.page(page)
@@ -53,10 +58,11 @@ def dashboard(request):
         # If page is out of range (e.g. 9999), deliver last page of results.
         equipos = paginator.page(paginator.num_pages)
 
-    return render_to_response('dashboard.html',{"titulo_modulo": titulo_modulo, "equipos": equipos}, context_instance=RequestContext(request))
+    usuario = request.user.username
+    return render_to_response('dashboard.html',{"usuario" : usuario, "titulo_modulo": titulo_modulo, "equipos": equipos}, context_instance=RequestContext(request))
    
 @login_required
-def medicionesxequipo(request, id):
+def medicionesxequipo(request, id = None):
 	#print "equipo_id" + str(id)
 	#try:
 	equipo = Equipo.objects.get(pk = id)
@@ -67,7 +73,7 @@ def medicionesxequipo(request, id):
 	#    return HttpResponseRedirect(reverse(dashboard))
 
 @login_required
-def medicionesxequipoactual(request, id):
+def medicionesxequipoactual(request, id = None):
 	try:
 		if id:
 			equipo_mediciones = EquipoMedicion.objects.filter(equipo_id = id).order_by('-fecha_creacion')
@@ -85,7 +91,7 @@ def medicionesxequipoactual(request, id):
 	return HttpResponseRedirect(reverse(dashboard))
 
 @login_required
-def graficasxequipo(request, id):
+def graficasxequipo(request, id = None):
 	#try:
 	equipo_mediciones = EquipoMedicion.objects.filter(equipo_id = id).order_by('-fecha_creacion')
 		#titulo_modulo = "Graficas " + str(equipo_mediciones[0].equipo.nombre)
@@ -94,10 +100,34 @@ def graficasxequipo(request, id):
 	    #return HttpResponseRedirect(reverse(dashboard))
 
 @login_required
-def obtener_datos_medidos_equipo(request, id):
+def obtener_datos_medidos_equipo(request, id = None):
     equipo_mediciones = EquipoMedicion.objects.filter(equipo_id = id).order_by('-fecha_creacion')[:10]
     data = serializers.serialize("json", equipo_mediciones)
     return HttpResponse(data, content_type='application/json')
+
+@login_required
+def registrar_obtener_equipo(request, id = None):
+	equipo = None
+	error_msg = None
+	if request.method == 'POST':
+		form_equipo = EquipoForm(request.POST)
+		if form_equipo.is_valid():
+			try:
+				equipo = form_equipo.save()
+				response = redirect(reverse(registrar_obtener_equipo, args = [equipo.id]))
+				return response
+			except ApplicationError,e:
+				error_msg = str(e)
+				transaction.rollback()
+			except:
+				transaction.rollback()
+				raise
+	else:
+		form_equipo = EquipoForm()
+
+	usuario = request.user.username
+	parametros = {"titulo_modulo" : "Equipo", "usuario" : usuario, "form_equipo" : form_equipo}
+	return render_to_response('equipo.html',parametros, context_instance=RequestContext(request))
 	
 	
 	
